b9c60301ad9f170b1fa44d0ac44bd755601c3dd2
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2024-03-14 15:39:02 +0100

    feat: Recognize ERC20 token - MEED-3806 - Meeds-io/MIPs#118  (#10)
    This change will verify the erc20 token's name and symbol in the polygon blockchain.

---
+++ gamification-evm-services/src/main/java/io/meeds/gamification/evm/model/ERC20Token.java
@@ -0,0 +1,21 @@
+package io.meeds.gamification.evm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ERC20Token {
+
+  private String name;
+
+  private String symbol;
+
+  private BigInteger totalSupply;
+
+  private BigInteger decimals;
+}
