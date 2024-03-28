0cdfe58f09fd23c701a31d91815aac0bb82ce176
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2024-03-28 09:33:47 +0100

    feat: Automate the erc20 token transfers rewarding for any blockchain network used - MEED-4557 - Meeds-io/MIPs#118 (#12)
    This change will automate the erc20 token transfers rewarding for any blockchain network used.
    
     #feat: Select blockchain when only creating an action - MEED-4280 - Meeds-io/MIPs#118 (#19)

--- gamification-evm-services/src/main/java/io/meeds/evm/gamification/scheduling/task/ERC20TransferTask.java
+++ gamification-evm-services/src/main/java/io/meeds/evm/gamification/scheduling/task/ERC20TransferTask.java
@@ -13,20 +13,21 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package io.meeds.gamification.evm.scheduling.task;
+package io.meeds.evm.gamification.scheduling.task;
 
 import java.util.List;
 import java.util.Set;
 
 import io.meeds.common.ContainerTransactional;
+import io.meeds.evm.gamification.model.TokenTransferEvent;
+import io.meeds.evm.gamification.service.BlockchainService;
+import io.meeds.evm.gamification.utils.Utils;
 import io.meeds.gamification.constant.DateFilterType;
 import io.meeds.gamification.constant.EntityStatusType;
-import io.meeds.gamification.evm.model.EvmTrigger;
-import io.meeds.gamification.evm.service.EvmTriggerService;
-import io.meeds.gamification.evm.service.BlockchainService;
+import io.meeds.evm.gamification.model.EvmTrigger;
+import io.meeds.evm.gamification.service.EvmTriggerService;
 import io.meeds.gamification.model.RuleDTO;
 import io.meeds.gamification.model.filter.RuleFilter;
-import io.meeds.gamification.service.EventService;
 import io.meeds.gamification.service.RuleService;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -34,16 +35,12 @@ import org.exoplatform.commons.api.settings.SettingService;
 import org.exoplatform.commons.api.settings.data.Context;
 import org.exoplatform.commons.api.settings.data.Scope;
 import org.exoplatform.commons.api.settings.SettingValue;
-import org.exoplatform.services.listener.ListenerService;
 
-import io.meeds.gamification.evm.model.TokenTransferEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import static io.meeds.gamification.evm.utils.Utils.*;
 
 @Component
 public class ERC20TransferTask {
@@ -56,71 +53,68 @@ public class ERC20TransferTask {
   private static final String  SETTING_LAST_TIME_CHECK_KEY = "transferredTokenTransactionsCheck";
 
   @Autowired
-  private EventService                      eventService;
+  private SettingService    settingService;
 
   @Autowired
-  private SettingService                    settingService;
+  private BlockchainService blockchainService;
 
   @Autowired
-  private ListenerService                   listenerService;
+  private EvmTriggerService evmTriggerService;
 
   @Autowired
-  private BlockchainService                 blockchainService;
-
-  @Autowired
-  private EvmTriggerService                 evmTriggerService;
-
-  @Autowired
-  private RuleService                       ruleService;
+  private RuleService       ruleService;
 
   @ContainerTransactional
-  @Scheduled(cron = "15 * * * * *")
+  @Scheduled(cron = "0 * * * * *")
   public synchronized void listenTokenTransfer() {
     LOG.info("Start listening erc20 token transfers");
     try {
-      RuleFilter ruleFilter = new RuleFilter(true);
-      ruleFilter.setEventType(CONNECTOR_NAME);
-      ruleFilter.setStatus(EntityStatusType.ENABLED);
-      ruleFilter.setProgramStatus(EntityStatusType.ENABLED);
-      ruleFilter.setDateFilterType(DateFilterType.STARTED);
-      List<RuleDTO> rules = ruleService.getRules(ruleFilter, 0, -1);
-      List<RuleDTO> filteredRules = rules.stream()
-              .filter(r -> !r.getEvent().getProperties().isEmpty()
-                      && StringUtils.isNotBlank(r.getEvent().getProperties().get(CONTRACT_ADDRESS)))
-              .toList();
-      if (CollectionUtils.isNotEmpty(filteredRules)) {
-        filteredRules.forEach(rule -> {
-          String contractAddress = rule.getEvent().getProperties().get(CONTRACT_ADDRESS);
-          long lastBlock = blockchainService.getLastBlock();
-          long lastCheckedBlock = getLastCheckedBlock(contractAddress);
-          if (lastCheckedBlock == 0) {
-            // If this is the first time that it's started, save the last block as
-            // last checked one
-            saveLastCheckedBlock(lastBlock, contractAddress);
-            return;
-          }
-          Set<TokenTransferEvent> events = blockchainService.getTransferredTokensTransactions(lastCheckedBlock + 1,
-                                                                                              lastBlock,
-                                                                                              contractAddress);
+        RuleFilter ruleFilter = new RuleFilter(true);
+        ruleFilter.setEventType(Utils.CONNECTOR_NAME);
+        ruleFilter.setStatus(EntityStatusType.ENABLED);
+        ruleFilter.setProgramStatus(EntityStatusType.ENABLED);
+        ruleFilter.setDateFilterType(DateFilterType.STARTED);
+        List<RuleDTO> rules = ruleService.getRules(ruleFilter, 0, -1);
+        List<RuleDTO> filteredRules = rules.stream()
+                .filter(r -> !r.getEvent().getProperties().isEmpty()
+                        && StringUtils.isNotBlank(r.getEvent().getProperties().get(Utils.CONTRACT_ADDRESS)))
+                .toList();
+        if (CollectionUtils.isNotEmpty(filteredRules)) {
+          filteredRules.forEach(rule -> {
+            String blockchainNetwork = rule.getEvent().getProperties().get(Utils.BLOCKCHAIN_NETWORK);
+            String contractAddress = rule.getEvent().getProperties().get(Utils.CONTRACT_ADDRESS);
+            long lastBlock = blockchainService.getLastBlock(blockchainNetwork);
+            long lastCheckedBlock = getLastCheckedBlock(contractAddress);
+            if (lastCheckedBlock == 0) {
+              // If this is the first time that it's started, save the last block as
+              // last checked one
+              saveLastCheckedBlock(lastBlock, contractAddress);
+              return;
+            }
+            Set<TokenTransferEvent> events = blockchainService.getTransferredTokensTransactions(lastCheckedBlock + 1,
+                                                                                                lastBlock,
+                                                                                                contractAddress,
+                                                                                                blockchainNetwork);
           if (!CollectionUtils.isEmpty(events)) {
             events.forEach(event -> {
               try {
                 EvmTrigger evmTrigger = new EvmTrigger();
-                evmTrigger.setTrigger(HOLD_TOKEN_EVENT);
-                evmTrigger.setType(CONNECTOR_NAME);
+                evmTrigger.setTrigger(Utils.HOLD_TOKEN_EVENT);
+                evmTrigger.setType(Utils.CONNECTOR_NAME);
                 evmTrigger.setWalletAddress(event.getTo());
                 evmTrigger.setTransactionHash(event.getTransactionHash());
                 evmTrigger.setContractAddress(contractAddress);
+                evmTrigger.setBlockchainNetwork(blockchainNetwork);
                 evmTriggerService.handleTriggerAsync(evmTrigger);
               } catch (Exception e) {
                 LOG.warn("Error broadcasting event '" + event, e);
               }
             });
           }
-          saveLastCheckedBlock(lastBlock, contractAddress);
-          LOG.info("End listening erc20 token transfers");
-        });
-      }
+            saveLastCheckedBlock(lastBlock, contractAddress);
+            LOG.info("End listening erc20 token transfers");
+          });
+        }
     } catch (Exception e) {
       LOG.error("An error occurred while listening erc20 token transfers", e);
     }
