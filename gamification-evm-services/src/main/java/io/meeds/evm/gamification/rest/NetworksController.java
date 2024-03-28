c34f354802becaf13e8e80e3cf3772fa873948f7
MayTekayaa <mtekaya@exoplatform.com>
2024-03-28 09:11:23 +0100

    MEED-4557 Automate the token erc20 transfers rewarding for any blockchain network used

---
+++ gamification-evm-services/src/main/java/io/meeds/evm/gamification/rest/NetworksController.java
@@ -0,0 +1,36 @@
+package io.meeds.evm.gamification.rest;
+
+import java.util.Set;
+
+import io.meeds.evm.gamification.service.NetworksService;
+import io.meeds.evm.gamification.model.BlockchainNetwork;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/gamification/connectors/evm/networks")
+public class NetworksController {
+
+  @Autowired
+  NetworksService networksService;
+
+  @GetMapping
+  @Operation(summary = "Retrieves the list of networks", method = "GET")
+  @ApiResponse(responseCode = "200", description = "Request fulfilled")
+  @ApiResponse(responseCode = "404", description = "Not found")
+  public Set<BlockchainNetwork> getNetworks() {
+      try {
+          return networksService.getNetworks();
+      } catch (IOException e) {
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No networks found");
+      }
+  }
+}
