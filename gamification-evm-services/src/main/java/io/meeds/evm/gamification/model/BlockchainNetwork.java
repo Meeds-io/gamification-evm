6dd41c54e04d62675f97a34bc80485c34593586e
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2024-03-28 09:33:47 +0100

    feat: Automate the erc20 token transfers rewarding for any blockchain network used - MEED-4557 - Meeds-io/MIPs#118 (#12)
    This change will automate the erc20 token transfers rewarding for any blockchain network used.
    
     #feat: Select blockchain when only creating an action - MEED-4280 - Meeds-io/MIPs#118 (#19)

---
+++ gamification-evm-services/src/main/java/io/meeds/evm/gamification/model/BlockchainNetwork.java
@@ -0,0 +1,15 @@
+package io.meeds.evm.gamification.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlockchainNetwork {
+
+  private String name;
+
+  private String providerUrl;
+}
