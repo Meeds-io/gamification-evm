2e00addbfddbfaa0b55a7324f3da717eca80c256
MayTekayaa <mtekaya@exoplatform.com>
2024-11-27 17:33:05 +0100

    MEED-7676 Adjust the display of blockchain network

--- gamification-evm-webapp/src/main/webapp/vue-app/connectorEventExtensions/components/EvmEventDisplay.vue
+++ gamification-evm-webapp/src/main/webapp/vue-app/connectorEventExtensions/components/EvmEventDisplay.vue
@@ -105,9 +105,9 @@ export default {
     },
     networkImageUrl() {
       if (this.networkId === 137 || this.networkId === 80002) {
-        return '/gamification-evm/images/polygon.png';
+        return '/gamification-evm/images/polygonLogo.svg';
       } else {
-        return '/gamification-evm/images/EVM.png';
+        return '/gamification-evm/images/EethereumLogo.svg';
       }
     },
     addressLabel() {
