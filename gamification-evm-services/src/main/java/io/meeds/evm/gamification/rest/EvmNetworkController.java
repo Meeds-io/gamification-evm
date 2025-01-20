5bd9e042406d12bf1c04c55686c2e30946dc227d
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2025-01-20 08:09:27 +0100

    feat: Review the display of tokens when viewing action - MEED-7853 - Meeds-io/MIPs#118   (#86)
    This change will review the display of tokens when viewing action.

--- gamification-evm-services/src/main/java/io/meeds/evm/gamification/rest/EvmNetworkController.java
+++ gamification-evm-services/src/main/java/io/meeds/evm/gamification/rest/EvmNetworkController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class EvmNetworkController {
   EvmNetworkService evmNetworkService;
 
   @GetMapping
+  @Secured("users")
   @Operation(summary = "Retrieves the list of networks", method = "GET")
   @ApiResponse(responseCode = "200", description = "Request fulfilled")
   @ApiResponse(responseCode = "404", description = "Not found")
